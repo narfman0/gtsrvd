c523babf30b25093d6ace80dd9e8aacc1a9286cb
Jon Robison <narfman0@gmail.com>
2018-08-02 22:29:25 -0400

    -Add simple cli

--- gtsrvd/app.py
+++ gtsrvd/app.py
@@ -1,3 +1,13 @@
-# TODO create new record set https://boto3.readthedocs.io/en/latest/reference/services/route53.html#Route53.Client.change_resource_record_sets resolving to this ip
-# TODO use nginx/whatever to proxy connections to mounted port (generated)
-# TODO upon shutdown, remove record set
+from gtsrvd import aws, nginx
+
+cloud = aws  # "eventually" will be cloud agnostic (?)
+
+
+def create(domain, subdomain, port):
+    cloud.create_record(domain, subdomain)
+    nginx.create_proxy(domain, subdomain, port)
+
+
+def delete(domain, subdomain, port):
+    cloud.delete_record(domain, subdomain)
+    nginx.delete_proxy(domain, subdomain, port)
